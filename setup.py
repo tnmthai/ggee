9196606c285daface72daf39a6caba06a19aebfe
Thai Tran <31507337+tnmthai@users.noreply.github.com>
2024-01-15 15:33:01 +1300

    updated

--- setup.py
+++ setup.py
@@ -5,8 +5,5 @@ setup(
     version="0.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        'httplib2',
-        'ee',        
-    ],
+    
 )
